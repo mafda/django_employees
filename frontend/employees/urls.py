cdb1785796db464f0e693e06cfa18cb877dc76ee
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-07-19 14:07:25 -0300

    include views.postemployees

--- frontend/employees/urls.py
+++ frontend/employees/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.employeesList.as_view(), name="employees_list"),
+    path("", views.index, name="employees_list"),
+    path("post/ajax/friend", views.postEmployees, name="post_employee"),
 ]
