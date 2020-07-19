689765e9f8f261c932f0c7bf49c51ac72d7659ae
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-07-19 14:06:38 -0300

    include class employees form

--- frontend/employees/forms.py
+++ frontend/employees/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from .models import Employees
 
 
 class NameForm(forms.Form):
     your_name = forms.CharField(label="Your name", max_length=100)
 
+
+class EmployeesForm(forms.ModelForm):
+    class Meta:
+        model = Employees
+        fields = "__all__"
