5354d12fbd273563a604caf9410ff133d1a10c79
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-07-19 14:05:07 -0300

    include class employees

--- frontend/employees/models.py
+++ frontend/employees/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Employees(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    document = models.IntegerField(unique=True)
+    name = models.CharField(max_length=32)
+    lastname = models.CharField(max_length=32)
+    salary = models.FloatField()
+
+    def _str_(self):
+        return (
+            f"       id: {self.employee_id}\n"
+            f"full name: {self.name} {self.lastname}\n"
+            f" document: {self.document}\n"
+            f"   salary: {self.salary}\n"
+        )
